7482bc0a585393539ae324a5343b982cf6228eaa
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-12 16:04:39 +0530

    try to deploy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/todo")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
@@ -23,5 +23,5 @@ const taskRoutes = require("./routes/taskRoutes");
 app.use("/tasks", taskRoutes);
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
